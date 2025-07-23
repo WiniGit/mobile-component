9f8e6d75231ded85cb42791374c9a61f0535d5a8
Thuno <chienvd@wini.vn>
2025-07-23 17:18:23 +0700

    0.0.29

--- src/component/image/image.tsx
+++ src/component/image/image.tsx
@@ -1,36 +1,46 @@
-/* eslint-disable react-native/no-inline-styles */
 import React, { useEffect, useState } from 'react';
-import { View, Image, Pressable } from 'react-native';
+import { View, Image, Pressable, ViewStyle, ImageResizeMode } from 'react-native';
 import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
-import type { ImageProps } from 'react-native';
+import { SvgUri } from 'react-native-svg';
 
-interface WImageProps extends ImageProps {
-  sourceError?: any;
+interface WImageProps {
+  style?: ViewStyle;
+  uri?: string;
+  resizeMode?: ImageResizeMode
 }
 
-export const WImage = ({ source, sourceError, ...props }: WImageProps) => {
-  const isRemoteImage = typeof source === 'object';
-  const [isLoading, setIsLoading] = useState<boolean>(isRemoteImage);
+export const WImage = ({ uri, ...props }: WImageProps) => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [isError, setIsError] = useState<boolean>(false);
+  const [svgError, setSvgError] = useState<boolean>(false);
 
   useEffect(() => {
-    if (isRemoteImage) setIsError(false)
-  }, [isRemoteImage]);
+    setIsLoading(true)
+    setIsError(false)
+    setSvgError(false)
+  }, [uri]);
 
-  return <>
-    <Pressable pointerEvents="none">
-      <Image
-        {...props}
-        source={isError ? (sourceError ?? { uri: "https://cdn.jsdelivr.net/gh/WiniGit/icon-library@latest/outline/development/image-2.svg" }) : source}
-        style={isLoading ? { position: 'absolute', opacity: 0 } : props.style}
-        onLoadEnd={() => setIsLoading(false)}
-        onError={() => setIsError(true)}
-      />
+  return uri?.length ? <>
+    <Pressable pointerEvents="none" style={props.style}>
+      {(uri.endsWith("svg") || isError) ?
+        <SvgUri
+          uri={svgError ? "https://cdn.jsdelivr.net/gh/WiniGit/icon-library@latest/outline/development/image-2.svg" : uri}
+          style={isLoading ? { position: 'absolute', opacity: 0 } : { width: "100%", height: "100%" }}
+          onLoad={() => setIsLoading(false)}
+          onError={() => setSvgError(true)}
+        /> :
+        <Image
+          source={{ uri: uri }}
+          resizeMode={props.resizeMode}
+          style={isLoading ? { position: 'absolute', opacity: 0 } : { width: "100%", height: "100%" }}
+          onLoadEnd={() => setIsLoading(false)}
+          onError={() => setIsError(true)}
+        />}
+      {isLoading && (
+        <SkeletonPlaceholder backgroundColor="#e0e0e0" highlightColor="#f5f5f5">
+          <View style={{ width: "100%", height: "100%" }} />
+        </SkeletonPlaceholder>
+      )}
     </Pressable>
-    {isLoading && (
-      <SkeletonPlaceholder backgroundColor="#e0e0e0" highlightColor="#f5f5f5">
-        <View style={props.style} />
-      </SkeletonPlaceholder>
-    )}
-  </>
+  </> : <SvgUri uri={"https://cdn.jsdelivr.net/gh/WiniGit/icon-library@latest/outline/development/image-2.svg"} />
 };
