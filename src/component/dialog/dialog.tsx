e187007e8ffe5d6ec054efe47d327115f6208be6
Thuno <thuno2698@gmail.com>
2025-10-15 11:57:39 +0700

    0.0.92

--- src/component/dialog/dialog.tsx
+++ src/component/dialog/dialog.tsx
@@ -1,11 +1,10 @@
 import React, { createRef, useMemo } from 'react';
 import { ComponentStatus, getStatusIcon } from '../component-status';
-import { KeyboardAvoidingView, Modal, Platform, Pressable, StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Modal, Platform, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { lightThemeColor } from '../../skin/color';
 import { useDesignTokens } from '../../module/WiniProvider';
 import { useTranslation } from 'react-i18next';
 import { WButton, WButtonVariant } from '../button/button';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 interface DialogState {
   readonly open?: boolean;
@@ -17,6 +16,7 @@ interface DialogState {
   onSubmit: Function;
   onCancel?: Function;
   submitTitle?: string;
+  small?: boolean;
 }
 
 export const showDialog = ({
@@ -26,6 +26,7 @@ export const showDialog = ({
   onSubmit,
   onCancel,
   submitTitle,
+  small
 }: {
   title?: string;
   status?: ComponentStatus;
@@ -35,14 +36,16 @@ export const showDialog = ({
   onSubmit?: Function;
   onCancel?: Function;
   submitTitle?: string;
+  small?: boolean;
 }) => {
   dialogRef.current.showDialogNoti({
-    title: title,
-    status: status,
-    content: content,
     onSubmit: onSubmit ?? (() => { }),
     onCancel: onCancel ?? (() => { }),
-    submitTitle: submitTitle,
+    title,
+    status,
+    content,
+    submitTitle,
+    small
   });
 };
 
@@ -65,22 +68,38 @@ export class CustomDialog extends React.Component<Object, DialogState> {
 
   render() {
     return <Modal animationType="slide" visible={this.state.open ?? false} transparent>
-      <View style={styles.overlay}>
-        <Container
-          status={this.state.status}
-          title={this.state.title}
-          content={this.state.content}
-          titleSubmit={this.state.submitTitle}
-          onSubmit={() => {
-            if (this.state.onSubmit) this.state.onSubmit();
-            this.closeDialog();
-          }}
-          titleCancel={this.state.titleCancel}
-          onCancel={() => {
-            if (this.state.onCancel) this.state.onCancel();
-            this.closeDialog();
-          }}
-        />
+      <View style={[styles.overlay, { padding: this.state.small ? 32 : 16 }]}>
+        {this.state.small ?
+          <SmallContainer
+            status={this.state.status}
+            title={this.state.title}
+            content={this.state.content}
+            titleSubmit={this.state.submitTitle}
+            onSubmit={() => {
+              if (this.state.onSubmit) this.state.onSubmit();
+              this.closeDialog();
+            }}
+            titleCancel={this.state.titleCancel}
+            onCancel={() => {
+              if (this.state.onCancel) this.state.onCancel();
+              this.closeDialog();
+            }}
+          /> :
+          <Container
+            status={this.state.status}
+            title={this.state.title}
+            content={this.state.content}
+            titleSubmit={this.state.submitTitle}
+            onSubmit={() => {
+              if (this.state.onSubmit) this.state.onSubmit();
+              this.closeDialog();
+            }}
+            titleCancel={this.state.titleCancel}
+            onCancel={() => {
+              if (this.state.onCancel) this.state.onCancel();
+              this.closeDialog();
+            }}
+          />}
       </View>
     </Modal>
   }
@@ -91,7 +110,50 @@ export const WDialog = () => {
   return <CustomDialog ref={dialogRef} />
 }
 
-const Container = ({ status = ComponentStatus.INFOR, ...props }: { status?: ComponentStatus, title?: string, content?: string | React.ReactNode, onCancel?: () => void, onSubmit?: () => void, titleCancel?: string, titleSubmit?: string }) => {
+interface ContainerProps {
+  status?: ComponentStatus;
+  title?: string;
+  content?: string | React.ReactNode;
+  onCancel?: () => void;
+  onSubmit?: () => void;
+  titleCancel?: string;
+  titleSubmit?: string;
+}
+
+const SmallContainer = ({ status = ComponentStatus.INFOR, ...props }: ContainerProps) => {
+  const { colors, textStyles } = useDesignTokens()
+  const { t } = useTranslation()
+  const mainColor = useMemo(() => {
+    switch (status) {
+      case ComponentStatus.WARNING:
+        return lightThemeColor['warning-color-main'];
+      case ComponentStatus.ERROR:
+        return lightThemeColor['error-color-main'];
+      case ComponentStatus.SUCCSESS:
+        return lightThemeColor['success-color-main'];
+      default: // ComponentStatus.INFOR
+        return lightThemeColor['infor-color-main'];
+    }
+  }, [status])
+
+  return <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} keyboardVerticalOffset={Platform.OS === 'ios' ? 35 : 0} style={[styles.smallContainer, { backgroundColor: colors?.['neutral-background-color-absolute'], borderColor: colors?.['neutral-border-color-main'] }]}>
+    <View style={{ gap: 8, padding: 16, alignItems: "center" }}>
+      {!!props.title?.length && <Text style={textStyles?.['heading-7']}>{props.title}</Text>}
+      {typeof props.content === 'string' ? <Text style={textStyles?.["subtitle-4"]}>{props.content}</Text> : props.content}
+    </View>
+    <View style={[styles.smallFooter, { borderTopColor: colors?.['neutral-border-color-main'] }]}>
+      <TouchableOpacity onPress={props.onCancel} style={styles.actionBtn}>
+        <Text style={[textStyles?.['label-4']]}>{props.titleCancel ?? t('cancel')}</Text>
+      </TouchableOpacity>
+      <View style={{ width: 1, height: 32, backgroundColor: colors?.['neutral-border-color-main'] }} />
+      <TouchableOpacity onPress={props.onSubmit} style={styles.actionBtn}>
+        <Text style={[textStyles?.['label-4'], { color: mainColor }]}>{props.titleSubmit ?? t('submit')}</Text>
+      </TouchableOpacity>
+    </View>
+  </KeyboardAvoidingView>
+}
+
+const Container = ({ status = ComponentStatus.INFOR, ...props }: ContainerProps) => {
   const { colors, textStyles } = useDesignTokens()
   const { t } = useTranslation()
   const mainColor = useMemo(() => {
@@ -151,7 +213,6 @@ const styles = StyleSheet.create({
     height: '100%',
     width: '100%',
     flex: 1,
-    padding: 16,
     justifyContent: 'center',
     backgroundColor: '#00000080',
   },
@@ -161,4 +222,22 @@ const styles = StyleSheet.create({
     padding: 24,
     gap: 16
   },
+  smallContainer: {
+    width: '100%',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderStyle: "solid",
+    overflow: "hidden"
+  },
+  smallFooter: {
+    flexDirection: "row",
+    borderTopWidth: 1,
+    borderStyle: "solid",
+  },
+  actionBtn: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 32,
+    flex: 1
+  }
 });
