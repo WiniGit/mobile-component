9be8f1f3c0e0886e1fce3f00e965fd0a5ac52f99
Thunt <135118868+WiniGit@users.noreply.github.com>
2025-10-21 14:49:57 +0700

    ...

--- src/component/bottom-sheet/bottom-sheet.tsx
+++ src/component/bottom-sheet/bottom-sheet.tsx
@@ -1,5 +1,5 @@
 import React, { forwardRef, RefObject, useImperativeHandle, useRef, useState } from 'react';
-import { View, Text, StyleSheet, Modal, Pressable, Animated, PanResponder, KeyboardAvoidingView, Platform, DimensionValue, TouchableWithoutFeedback, ViewStyle, useWindowDimensions } from 'react-native';
+import { View, Text, StyleSheet, Modal, Pressable, Animated, PanResponder, DimensionValue, TouchableWithoutFeedback, ViewStyle, useWindowDimensions } from 'react-native';
 import { useDesignTokens } from '../../module/WiniProvider';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -52,39 +52,37 @@ export const WBottomSheet = forwardRef<BottomSheetRef, any>((_, ref) => {
   const returnModal = () => {
     // Thêm statusBarTranslucent để ngăn modal cha nhận sự kiện khi modal con hiển thị
     return <Modal transparent visible={isVisible} animationType="slide" statusBarTranslucent={true} >
-      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined} style={{ flex: 1 }}>
-        <TouchableWithoutFeedback style={{ flex: 1 }}>
-          <Container onDismiss={btmSheetState.enableDismiss ? onDismiss : undefined}>
+      <TouchableWithoutFeedback style={{ flex: 1 }}>
+        <Container onDismiss={btmSheetState.enableDismiss ? onDismiss : undefined}>
+          <View
+            style={[
+              styles.container,
+              { backgroundColor: colors?.['neutral-background-color-absolute'] },
+            ]}
+            pointerEvents="box-none"
+          >
             <View
-              style={[
-                styles.container,
-                { backgroundColor: colors?.['neutral-background-color-absolute'] },
-              ]}
-              pointerEvents="box-none"
-            >
-              <View
-                style={{
-                  width: 56,
-                  marginTop: 8,
-                  height: 6,
-                  borderRadius: 10,
-                  backgroundColor: colors?.['neutral-background-color-bolder'],
-                }}
-              />
-              <Pressable style={[{ width: '100%' }, btmSheetState.style]}>
-                {!!btmSheetState.title?.length && (
-                  <View style={[styles.header, { borderBottomColor: colors?.['neutral-border-color-main'], justifyContent: (btmSheetState.prefixAction && btmSheetState.suffixAction) ? "space-between" : btmSheetState.suffixAction ? "flex-end" : "flex-start" }]}>
-                    <Text style={[textStyles?.['heading-7'], styles.title]}>{btmSheetState.title}</Text>
-                    {btmSheetState.prefixAction}
-                    {btmSheetState.suffixAction}
-                  </View>
-                )}
-                {btmSheetState.children}
-              </Pressable>
-            </View>
-          </Container>
-        </TouchableWithoutFeedback>
-      </KeyboardAvoidingView>
+              style={{
+                width: 56,
+                marginTop: 8,
+                height: 6,
+                borderRadius: 10,
+                backgroundColor: colors?.['neutral-background-color-bolder'],
+              }}
+            />
+            <Pressable style={[{ width: '100%' }, btmSheetState.style]}>
+              {!!btmSheetState.title?.length && (
+                <View style={[styles.header, { borderBottomColor: colors?.['neutral-border-color-main'], justifyContent: (btmSheetState.prefixAction && btmSheetState.suffixAction) ? "space-between" : btmSheetState.suffixAction ? "flex-end" : "flex-start" }]}>
+                  <Text style={[textStyles?.['heading-7'], styles.title]}>{btmSheetState.title}</Text>
+                  {btmSheetState.prefixAction}
+                  {btmSheetState.suffixAction}
+                </View>
+              )}
+              {btmSheetState.children}
+            </Pressable>
+          </View>
+        </Container>
+      </TouchableWithoutFeedback>
     </Modal>
   }
 
@@ -122,7 +120,7 @@ const Container = (props: { children: React.ReactNode; onDismiss?: () => void })
         if (gestureState.vy > velocityThreshold && gestureState.dy > 50) {
           Animated.timing(pan.y, {
             toValue: scrSize.height,
-            duration: 300,
+            duration: 50,
             useNativeDriver: false,
           }).start(props.onDismiss);
         } else {
